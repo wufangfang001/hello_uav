f8a2c12a372d38d60f0908da614b73c444e3bfa8
shangjinlong <shangjinlong@agora.io>
2025-11-05 15:09:26 +0800

    [FIX] remove x86_64 file

--- build-x86_64.sh
+++
@@ -1,5 +0,0 @@
-gcc -O2 -fsanitize=address -fno-omit-frame-pointer -g -o hello-uav \
-    src/hello_uav.c \
-    src/video_capture.c \
-    src/h264_encode.c \
-    -Iinc -Llib/x86_64 -Wl,-rpath,lib/x86_64 -lagora-rtc-sdk -lpthread -lasan
